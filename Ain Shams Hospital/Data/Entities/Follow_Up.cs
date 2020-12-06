b272aeeca32f6f618acff11d9e576396e0cef608
Heba-Atef99 <hoba30.ha@gmail.com>
2020-12-07 01:49:04 +0200

    heba four classes done

---
+++ Ain Shams Hospital/Data/Entities/Follow_Up.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ain_Shams_Hospital.Data.Entities
+{
+    public class Follow_Up
+    {
+        public int Id { get; set; }
+        public Staff Staff_Id { get; set; }
+        public Patient Patient_Id { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ Ain Shams Hospital/Data/Entities/Staff_Schedule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ain_Shams_Hospital.Data.Entities
+{
+    public class Staff_Schedule
+    {
+        public Staff Staff_Id { get; set; }
+        public string Working_Day { get; set; }
+
+    }
+}
