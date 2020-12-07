10449ef3e8fe1a09707fb65dade77b9d8ebc106f
nermeen_atif <1701571@eng.asu.edu.eg>
2020-12-07 15:28:19 +0200

    change in data file

--- Ain Shams Hospital/Data/Entities/Follow_Up.cs
+++ Ain Shams Hospital/Data/Entities/Follow_Up.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Ain_Shams_Hospital.Models.Entities
+namespace Ain_Shams_Hospital.Data.Entities
 {
     public class Follow_Up
     {
--- Ain Shams Hospital/Data/Entities/Registration.cs
+++ Ain Shams Hospital/Data/Entities/Registration.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Ain_Shams_Hospital.Models.Entities
+namespace Ain_Shams_Hospital.Data.Entities
 {
     public class Registration
     {
