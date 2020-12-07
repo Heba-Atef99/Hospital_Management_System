c70f7cefb0a097f4ff540979b81ce01198ec4742
Ahmed <ahmedanis03@gmail.com>
2020-12-07 14:11:29 +0200

    add five classes and create data folder

---
+++ Ain Shams Hospital/Data/Entities/Follow_Up_Type.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ain_Shams_Hospital.Data.Entities
+{
+    public class Follow_Up_Type
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+    }
+}
