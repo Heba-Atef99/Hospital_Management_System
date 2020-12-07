0858e2546a614225a97cc5aa0ff0e478627c5176
nouraan-ahmed <52378608+nouraan-ahmed@users.noreply.github.com>
2020-12-07 19:00:25 +0200

    Using Configurations + Migration(Adding Tables)

--- Ain Shams Hospital/Data/Entities/Staff_Schedule.cs
+++ Ain Shams Hospital/Data/Entities/Staff_Schedule.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Ain_Shams_Hospital.Data.Entities
 {
+    [Keyless]
     public class Staff_Schedule
     {
         public Staff Staff_Id { get; set; }
--- Ain Shams Hospital/Startup.cs
+++ Ain Shams Hospital/Startup.cs
@@ -1,6 +1,8 @@
+using HospitalManagementSystem.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,10 @@ namespace Ain_Shams_Hospital
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<HospitalDbContext>(cfg=>
+            {
+                cfg.UseSqlServer(Configuration.GetConnectionString("HospitalConnectionString"));
+            });
             services.AddControllersWithViews();
         }
 
