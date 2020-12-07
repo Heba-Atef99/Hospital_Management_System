542f4fcce16d8cf50203d2414637c369e59363d1
nermeen_atif <1701571@eng.asu.edu.eg>
2020-12-07 15:14:21 +0200

    add five classes and data folder

---
+++ Ain Shams Hospital/Data/Entities/Follow_Up_History.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ain_Shams_Hospital.Data.Entities
+{
+    public class Follow_Up_History
+    {
+        public Follow_Up Follow_Up_Id { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+        public Follow_Up_Type Type { get; set; }
+    }
+}
