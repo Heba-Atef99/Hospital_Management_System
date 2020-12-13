bef4d62881c78f0c782b241d5cc4e89b0802c2a1
Heba-Atef99 <hoba30.ha@gmail.com>
2020-12-13 10:47:51 +0200

    cahnged class staff scheduel & seeding facilities, transfer hospitals

--- Ain Shams Hospital/Data/Entities/Staff_Schedule.cs
+++ Ain Shams Hospital/Data/Entities/Staff_Schedule.cs
@@ -9,7 +9,7 @@ namespace Ain_Shams_Hospital.Data.Entities
     [Keyless]
     public class Staff_Schedule
     {
-        public Specialization Specialization_ { get; set; }
+        public Staff Staff_ { get; set; }
         public string Working_Day { get; set; }
 
     }
---
+++ Ain Shams Hospital/Data/Migrations/20201213083813_new.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Ain_Shams_Hospital.Migrations
+{
+    public partial class @new : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Staff_Schedules_Specializations_Specialization_Id",
+                table: "Staff_Schedules");
+
+            migrationBuilder.RenameColumn(
+                name: "Specialization_Id",
+                table: "Staff_Schedules",
+                newName: "Staff_Id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Staff_Schedules_Specialization_Id",
+                table: "Staff_Schedules",
+                newName: "IX_Staff_Schedules_Staff_Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Staff_Schedules_Staff_Staff_Id",
+                table: "Staff_Schedules",
+                column: "Staff_Id",
+                principalTable: "Staff",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Staff_Schedules_Staff_Staff_Id",
+                table: "Staff_Schedules");
+
+            migrationBuilder.RenameColumn(
+                name: "Staff_Id",
+                table: "Staff_Schedules",
+                newName: "Specialization_Id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Staff_Schedules_Staff_Id",
+                table: "Staff_Schedules",
+                newName: "IX_Staff_Schedules_Specialization_Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Staff_Schedules_Specializations_Specialization_Id",
+                table: "Staff_Schedules",
+                column: "Specialization_Id",
+                principalTable: "Specializations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
