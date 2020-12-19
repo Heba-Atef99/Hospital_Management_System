cab66d46ab6d850e18deef044d5e18c5eea5ce8c
Heba-Atef99 <hoba30.ha@gmail.com>
2020-12-19 20:57:43 +0200

    add new files

---
+++ Ain Shams Hospital 2/Data/Entities/Follow_Up.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ain_Shams_Hospital.Data.Entities
+{
+    public class Follow_Up
+    {
+        public int Id { get; set; }
+        public Staff Staff_ { get; set; }
+        public Patient Patient_ { get; set; }
+        public string Status { get; set; }
+    }
+}
