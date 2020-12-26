4cf767e6dc1275e7e0954e9b8c95eac94db9e86e
nouraan-ahmed <52378608+nouraan-ahmed@users.noreply.github.com>
2020-12-27 00:07:30 +0200

    Adding Forms To Doctor Types

---
+++ Ain Shams Hospital/Controllers/PatientController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ain_Shams_Hospital.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult labSpecialist()
+        {
+            return View();
+        }
+        public IActionResult EyesDoctor()
+        {
+            return View();
+        }
+        public IActionResult  BabyDoctor()
+        {
+            return View();
+        }
+        public IActionResult Surgeon()
+        {
+            return View();
+        }
+    }
+}
