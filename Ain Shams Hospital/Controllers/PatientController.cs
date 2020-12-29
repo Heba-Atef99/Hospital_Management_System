89b98e0f54df7ce404b483b0ad432e276f263043
iamRenadMostafa <renad.mostafa333@gmail.com>
2020-12-29 22:48:33 +0200

    new modefication on eyedoctors

--- Ain Shams Hospital/Controllers/PatientController.cs
+++ Ain Shams Hospital/Controllers/PatientController.cs
@@ -1,46 +1,49 @@
 ﻿using Ain_Shams_Hospital.Data.Entities;
-using Ain_Shams_University.ViewModels;
+using Ain_Shams_Hospital.Models;
 using HospitalManagementSystem.Data;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using Ain_Shams_Hospital.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ain_Shams_Hospital.Controllers
 {
     public class PatientController : Controller
     {
-        private readonly HospitalDbContext _HDB;
 
-        public PatientController(HospitalDbContext HDB)
+        private readonly HospitalDbContext _auc;
+        public PatientController (HospitalDbContext auc)
         {
-            _HDB = HDB;
+            _auc = auc;
         }
 
         public IActionResult labSpecialist()
         {
             return View();
         }
-        public IActionResult EyesDoctor()
+        public IActionResult EyesDoctor(RegistrationStaffVM objc)
         {
+            Staff name = new Staff();
+            var EmailExist = _auc.Staff.ToList().Any(u => u.Specialization_Id ==12);
+            if (EmailExist)
+            {
+                List<Staff> s1 = new List<Staff>();
+                s1 = (from s in _auc.Staff select s).ToList();
+                s1.Insert(0, new Staff { Id = 0, Name = "--selsct your doctor--" });
+                ViewBag.massege = s1;
+                return View();
+
+            }
             return View();
         }
         public IActionResult  BabyDoctor()
         {
-            return View();
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult BabyDoctor(BabyDoctorVM bd)
-        {
-            Follow_Up_History fup = new Follow_Up_History();
-            fup.Date = bd.Date;
-            fup.Follow_Up_Type_Id = 2;
-
-            _HDB.Add(fup);
-            _HDB.SaveChanges();
-            ViewBag.message = "Your Time has been recorded";
             return View();
         }
         public IActionResult Surgeon()
