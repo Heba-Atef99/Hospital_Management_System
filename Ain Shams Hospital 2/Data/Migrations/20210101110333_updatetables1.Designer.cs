b0706d9f22056ac48ee038c8e4fcf8920f3266ce
nouraan-ahmed <52378608+nouraan-ahmed@users.noreply.github.com>
2021-01-01 21:05:09 +0200

    Update_DBTables&addingMigrations

--- Ain Shams Hospital 2/Data/Migrations/20210101110333_updatetables1.Designer.cs
+++ Ain Shams Hospital 2/Data/Migrations/20210101110333_updatetables1.Designer.cs
@@ -4,14 +4,16 @@ using HospitalManagementSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Ain_Shams_Hospital.Migrations
 {
     [DbContext(typeof(HospitalDbContext))]
-    partial class HospitalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210101110333_updatetables1")]
+    partial class updatetables1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
