519cb9db1b405c02a90d6a50014e9f2f2a24a02e
nouraan-ahmed <52378608+nouraan-ahmed@users.noreply.github.com>
2021-01-02 02:20:16 +0200

    adding the views of the doctors

--- Ain Shams Hospital/Controllers/PatientController.cs
+++ Ain Shams Hospital/Controllers/PatientController.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace Ain_Shams_Hospital.Controllers
 {
@@ -23,9 +24,23 @@ namespace Ain_Shams_Hospital.Controllers
 
             List<Staff> s1 = new List<Staff>();
             s1 = (from s in _HDB.Staff select s).Where(f => f.Specialization_Id == 22).ToList();
-            s1.Insert(0, new Staff { Id = 0, Name = "--selsct your doctor--" });
+            s1.Insert(0, new Staff { Id = 0, Name = "--select your doctor--" });
             ViewBag.massege = s1;
             return View();
+           
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult labSpecialist(labSpecialistVM bd)
+        {
+
+            Follow_Up_History fuph = new Follow_Up_History();
+            fuph.Date = bd.Date;
+            fuph.Follow_Up_Type_Id = 2;
+
+            _HDB.Add(fuph);
+            _HDB.SaveChanges();
+            ViewBag.message = "Your Time has been recorded";
             return View();
         }
         public IActionResult EyesDoctor(Specialization obj)
@@ -33,16 +48,30 @@ namespace Ain_Shams_Hospital.Controllers
            
                 List<Staff> s1 = new List<Staff>();
                 s1 = (from s in _HDB.Staff select s).Where(f=>f.Specialization_Id==12).ToList();
-                s1.Insert(0, new Staff { Id = 0, Name = "--selsct your doctor--" });
+                s1.Insert(0, new Staff { Id = 0, Name = "--select your doctor--" });
                 ViewBag.massege = s1;
                 return View();
        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EyesDoctor(EyesDoctorVM ed)
+        {
+
+            Follow_Up_History fuph = new Follow_Up_History();
+            fuph.Date = ed.Date;
+            fuph.Follow_Up_Type_Id = 2;    //m7tageen n5aleeh y5tar anhy test hy3mlo
+
+            _HDB.Add(fuph);
+            _HDB.SaveChanges();
+            ViewBag.message = "Your Time has been recorded";
+            return View();
+        }
         public IActionResult  BabyDoctor()
         {
 
             List<Staff> s1 = new List<Staff>();
             s1 = (from s in _HDB.Staff select s).Where(f => f.Specialization_Id == 10).ToList();
-            s1.Insert(0, new Staff { Id = 0, Name = "--selsct your doctor--" });
+            s1.Insert(0, new Staff { Id = 0, Name = "--select your doctor--" });
             ViewBag.massege = s1;
            
             return View();
@@ -51,11 +80,12 @@ namespace Ain_Shams_Hospital.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult BabyDoctor(BabyDoctorVM bd)
         {
-            Follow_Up_History fup = new Follow_Up_History();
-            fup.Date = bd.Date;
-            fup.Follow_Up_Type_Id = 2;
+            
+            Follow_Up_History fuph = new Follow_Up_History();
+            fuph.Date = bd.Date;
+            fuph.Follow_Up_Type_Id = 2;
 
-            _HDB.Add(fup);
+            _HDB.Add(fuph);
             _HDB.SaveChanges();
             ViewBag.message = "Your Time has been recorded";
             return View();
@@ -65,9 +95,23 @@ namespace Ain_Shams_Hospital.Controllers
 
             List<Staff> s1 = new List<Staff>();
             s1 = (from s in _HDB.Staff select s).Where(f => f.Specialization_Id == 9).ToList();
-            s1.Insert(0, new Staff { Id = 0, Name = "--selsct your doctor--" });
+            s1.Insert(0, new Staff { Id = 0, Name = "--select your doctor--" });
             ViewBag.massege = s1;
             return View();
+            
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Surgeon(SurgeonVM bd)
+        {
+
+            Follow_Up_History fuph = new Follow_Up_History();
+            fuph.Date = bd.Date;
+            fuph.Follow_Up_Type_Id = 1;
+
+            _HDB.Add(fuph);
+            _HDB.SaveChanges();
+            ViewBag.message = "Your Time has been recorded";
             return View();
         }
     }
