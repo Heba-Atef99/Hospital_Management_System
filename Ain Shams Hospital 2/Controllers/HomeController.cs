9e21b461d0c7eb80c5145b824b22512d7651102f
iamRenadMostafa <renad.mostafa333@gmail.com>
2021-01-07 21:52:39 +0200

    donation feature is added

--- Ain Shams Hospital 2/Controllers/HomeController.cs
+++ Ain Shams Hospital 2/Controllers/HomeController.cs
@@ -6,17 +6,30 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Ain_Shams_Hospital.ViewModels;
+using Ain_Shams_University.ViewModels;
+using Ain_Shams_Hospital.Data.Entities;
+using HospitalManagementSystem.Data;
+using Microsoft.AspNetCore.Http;
+
 
 namespace Ain_Shams_Hospital.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly HospitalDbContext _HDB;
+
+        public HomeController(HospitalDbContext HDB)
+        {
+            _HDB = HDB;
+        }
+
+       /* private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-        }
+        }*/
 
         public IActionResult Index()
         {
@@ -32,6 +45,36 @@ namespace Ain_Shams_Hospital.Controllers
             return View();
         }
         public IActionResult Covid()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Donation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Donation(DonationsVM don)
+        {
+             
+            Donation dona = new Donation();
+            dona.Name = don.Name;
+            dona.Email = don.Email;
+            dona.Day = don.Day;
+            dona.Phone_Number = don.PhoneNumber;
+            _HDB.Add(dona);
+            _HDB.SaveChanges();
+            String visitor_name = dona.Name;
+            ViewBag.visitorname = visitor_name;
+            return RedirectToAction("saveDonation", "Home");
+        }
+        [HttpGet]
+        public IActionResult saveDonation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult saveDonation(Donation d)
         {
             return View();
         }
---
+++ Ain Shams Hospital 2/ViewModels/Donation/DonationsVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ain_Shams_University.ViewModels
+{
+    public class DonationsVM
+    {
+        [Required(ErrorMessage = "Please Enter Your Name")]
+        public string  Name { get; set; }
+        [Required]
+        public string Day { get; set; }
+        [EmailAddress ]
+        public string Email { get; set; }
+        [Key]
+        public int Id { get; set; }
+        [Phone]
+        [StringLength(11)]
+        public string PhoneNumber { get; set; }
+    }
+}
