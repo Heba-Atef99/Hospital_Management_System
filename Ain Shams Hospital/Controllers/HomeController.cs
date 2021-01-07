42d96f30ee59b006603b645f5d6291b50ade49ec
iamRenadMostafa <renad.mostafa333@gmail.com>
2021-01-07 17:56:16 +0200

    Donation adding

--- Ain Shams Hospital/Controllers/HomeController.cs
+++ Ain Shams Hospital/Controllers/HomeController.cs
@@ -6,17 +6,30 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Ain_Shams_Hospital.ViewModels;
+using Ain_Shams_University.ViewModels;
+using Ain_Shams_Hospital.Data.Entities;
+using HospitalManagementSystem.Data;
+using Microsoft.AspNetCore.Http;
+
 
 namespace Ain_Shams_Hospital.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly HospitalDbContext _HDB;
+
+        public HomeController(HospitalDbContext HDB)
+        {
+            _HDB = HDB;
+        }
+
+       /* private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-        }
+        }*/
 
         public IActionResult Index()
         {
@@ -35,7 +48,26 @@ namespace Ain_Shams_Hospital.Controllers
         {
             return View();
         }
+        [HttpGet]
         public IActionResult Donation()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Donation(DonationsVM don)
+        {
+            Donation dona = new Donation();
+            dona.Name = don.Name;
+            dona.Email = don.Email;
+            dona.Day = don.Day;
+            dona.Phone_Number = don.PhoneNumber;
+            _HDB.Add(dona);
+            _HDB.SaveChanges();
+
+            return RedirectToAction("save","Home");
+        }
+        public IActionResult save()
         {
             return View();
         }
---
+++ Ain Shams Hospital/ViewModels/Donation/DonationsVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ain_Shams_University.ViewModels
+{
+    public class DonationsVM
+    {
+        [Required(ErrorMessage = "Please Choose Your Doctor")]
+        public string  Name { get; set; }
+        [Required]
+        [DataType(DataType.DateTime)]
+        public string Day { get; set; }
+        [EmailAddress ]
+        public string Email { get; set; }
+        [Key]
+        public int Id { get; set; }
+        [Phone]
+        [StringLength(11)]
+        public string PhoneNumber { get; set; }
+    }
+}
