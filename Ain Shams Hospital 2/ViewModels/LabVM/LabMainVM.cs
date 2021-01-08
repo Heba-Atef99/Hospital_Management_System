1eb9b117b099d6d76a1e495a9505c5622167ce33
nermeen_atif <1701571@eng.asu.edu.eg>
2021-01-08 20:16:58 +0200

    add labs

---
+++ Ain Shams Hospital 2/ViewModels/LabVM/LabMainVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ain_Shams_Hospital.ViewModels.LabVM
+{
+    public class LabMainVM
+    {
+        public int P_Id { get; set; }
+        public string Status { get; set; }
+        public string Search_Item { get; set; }
+    }
+}
