1cafd5e947cd25aa05abed7b0a61141a6d3654bf
rere-kh1999 <tatol_1965@hotmail.com>
2021-01-13 09:55:24 +0200

    Editing image for profile

---
+++ Ain Shams Hospital 2/ViewModels/PatientVM/PictureVM.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Ain_Shams_University.ViewModels
+{
+    public class PictureVM
+    {
+        [Required]
+        public IFormFile Picture { get; set; }
+    }
+
+    p
+}
