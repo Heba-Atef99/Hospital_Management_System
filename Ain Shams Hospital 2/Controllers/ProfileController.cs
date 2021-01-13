1b9ed48c3fb4318f26b7a64083f7a404a213dc43
nouraan-ahmed <norahahmed1999@gmail.com>
2021-01-13 10:25:35 +0200

    Revert "Edit"
    This reverts commit c917324136b0ba776f5f77977d5f901196c65daf.

--- Ain Shams Hospital 2/Controllers/ProfileController.cs
+++ Ain Shams Hospital 2/Controllers/ProfileController.cs
@@ -1,19 +1,18 @@
 ﻿using Ain_Shams_Hospital.Data.Entities;
 using Ain_Shams_University.ViewModels;
 using HospitalManagementSystem.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Web;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Ain_Shams_Hospital.ViewModels;
-using Microsoft.EntityFrameworkCore;
-using Ain_Shams_Hospital.Controllers;
 
 namespace Ain_Shams_University.Controllers
 {
-    [CheckXActionFilterAttribute]
     public class ProfileController : Controller
     {
         private readonly HospitalDbContext _HDB;
@@ -30,36 +29,41 @@ namespace Ain_Shams_University.Controllers
         public IActionResult UserProfile()
         {
             int Patient_Reg_Id = (int)HttpContext.Session.GetInt32("User_Reg_Id");
-            int patiant_Id = _HDB.Patients.Where(o => o.Registration_Id == Patient_Reg_Id).Select(i => i.Id).SingleOrDefault();
-
-            return View(_HDB.Patients.Find(patiant_Id));
+            int patient_Id = _HDB.Patients.Where(e => e.Registration_Id == Patient_Reg_Id).Select(a=>a.Id).SingleOrDefault();
+            return View(_HDB.Patients.Find(patient_Id));
         }
-
-        public IActionResult Edit(int id =0)
+        public IActionResult UpdatePicture(PictureVM obj)
         {
             int Patient_Reg_Id = (int)HttpContext.Session.GetInt32("User_Reg_Id");
-            int patiant_Id = _HDB.Patients.Where(o => o.Registration_Id == Patient_Reg_Id).Select(i => i.Id).SingleOrDefault();
-            ViewBag.pr = _HDB.Patients.Where(p => p.Id == patiant_Id).Select(d => d.Health_Progress).SingleOrDefault();
-            var hospital_id = _HDB.Patients.Where(p => p.Id == patiant_Id).Select(d => d.Hospital_Id).SingleOrDefault();
-            ViewBag.hos = _HDB.Transfer_Hospitals.Where(e => e.Id == hospital_id).Select(r => r.Name).SingleOrDefault();
-            Patient pa = _HDB.Patients.Find(id);
-            if (pa == null)
+            int patient_Id = _HDB.Patients.Where(e => e.Registration_Id == Patient_Reg_Id).Select(a => a.Id).SingleOrDefault();
+            if (patient_Id == 0)
             {
-                return NotFound();
+                return RedirectToAction("Login", "Registration");
             }
-            
-            return View(pa);
-        }
-        [HttpPost]
-        public IActionResult Edit(Patient pa)
-        {
-            if (ModelState.IsValid)
+            var file = obj.Picture;
+            Patient u = _HDB.Patients.Find(patient_Id);
+            if (file != null)
             {
-                _HDB.Entry(pa).State = EntityState.Modified;
+                var extension = Path.GetExtension(file.FileName);
+                string id_and_extension = patient_Id + extension;
+                string imgUrl = "~/Profile Images/" + id_and_extension;
+                u.Image = imgUrl;
+                _HDB.Entry(u).State = EntityState.Modified;
                 _HDB.SaveChanges();
-                return RedirectToAction("UserProfile");
+
+                var path = Server.MapPath("~/Profile Images/");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                if ((System.IO.File.Exists(path + id_and_extension)))
+                {
+                    System.IO.File.Delete(path + id_and_extension);
+                }
+                file.SaveAs((path + id_and_extension));
+
             }
-            return View(pa);
+            return RedirectToAction("UserProfile"); 
         }
     }
 }
