1f20649cbd4ccf9b63539c6bf5a7b5a6ef5d0e89
nouraan-ahmed <norahahmed1999@gmail.com>
2021-01-13 23:10:35 +0200

    Adding to the source code

---
+++ To be delivered/Source Code/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ain_Shams_Hospital.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Name { get; set; }
+    }
+}
